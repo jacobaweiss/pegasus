0659825136f8d1fd2d786e7e96cd36d0239624ed
Jack Weiss <jacobaweiss@gmail.com>
2016-07-14 16:43:51 -0700

    Add some tests, update project.clj

--- project.clj
+++ project.clj
@@ -1,6 +1,5 @@
 (defproject pegasus "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "Implementation of the peg game"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]]
--- test/pegasus/core_test.clj
+++ test/pegasus/core_test.clj
@@ -2,6 +2,53 @@
   (:require [clojure.test :refer :all]
             [pegasus.core :refer :all]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(def default-board (remove-peg (new-board 5) 4))
+
+(deftest test-triangular?
+  (testing "triangular?"
+    (is (= true (triangular? 6)))
+    (is (= false (triangular? 5)))))
+
+(deftest test-row-tri
+  (testing "row-tri"
+    (is (= 6 (row-tri 3)))
+    (is (= 10 (row-tri 4)))))
+
+(deftest test-row-num
+  (testing "row-num"
+    (is (= 1 (row-num 1)))
+    (is (= 2 (row-num 3)))
+    (is (= 5 (row-num 12)))))
+
+(deftest test-new-board
+  (testing "new-board"
+    (let [expected {:rows 3,
+                    1 {:pegged true, :connections {4 2, 6 3}},
+                    4 {:connections {1 2, 6 5}, :pegged true},
+                    6 {:connections {1 3, 4 5}, :pegged true},
+                    2 {:pegged true},
+                    3 {:pegged true},
+                    5 {:pegged true}}]
+      (is (= expected (new-board 3))))))
+
+(deftest test-valid-moves
+  (testing "valid-moves"
+      (is (= {4 2} (valid-moves default-board 1)))))
+
+(deftest test-make-move
+  (testing "make-move"
+    (let [board (remove-peg (new-board 3) 1)
+          expected {:rows 3,
+                    1 {:pegged true, :connections {4 2, 6 3}},
+                    4 {:connections {1 2, 6 5}, :pegged true},
+                    6 {:connections {1 3, 4 5}, :pegged false},
+                    2 {:pegged true},
+                    3 {:pegged false},
+                    5 {:pegged true}}]
+      (is (= expected (make-move board 6 1)))
+      (is (= nil (make-move board 6 2))))))
+
+(deftest test-can-move?
+  (testing "can-move?"
+    (is (= {4 2} (can-move? default-board)))
+    (is (= nil (can-move? (new-board 5))))))
